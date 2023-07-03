b5d459ef79aefc7f25fcad30df20d565a4861b76
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-07-03 12:48:08 +0300

    added build.gradle

---
+++ src/test/java/ya/lelikus81/FirstTest.java
@@ -0,0 +1,41 @@
+package ya.lelikus81;
+
+import org.junit.jupiter.api.*;
+
+public class FirstTest {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("This is the before all methods");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("This is the after all methods");
+    }
+
+    @BeforeEach
+    void openYandex(){
+        // open("yandex.ru")
+        System.out.print("open yandex.ru");
+    }
+
+    @AfterEach
+    void takeScreen(){
+        //take screen
+        System.out.println("take screen");
+
+    }
+
+    @Test
+    void firsTest (){
+        System.out.println("   first test");
+        Assertions.assertTrue(true);
+    }
+
+    @Test
+    void secondTest (){
+        System.out.println("   second test");
+        Assertions.assertTrue(true);
+    }
+}

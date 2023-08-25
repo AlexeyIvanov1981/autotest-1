33416360cf3c47e7ca74de7a360b783fff79ce0d
AlexeyIvanov1981 <lelikus81@yandex.ru>
2023-08-25 10:01:41 +0300

    added PracticeFotmTest.java

--- src/test/java/ya/lelikus81/PracticeFotmTest.java
+++ src/test/java/ya/lelikus81/PracticeFotmTest.java
@@ -10,14 +10,14 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class PracticeFotmTest {
     @BeforeAll
-    static void browserSize(){
+    static void browserSize() {
         Configuration.startMaximized = true;
         Configuration.browser = "FIREFOX";
 //        Configuration.browserSize = "1900x1240";
     }
 
     @Test
-    void fillAllForm(){
+    void fillAllForm() {
         open("https://demoqa.com/automation-practice-form");
 
         $("#firstName").setValue("Alex");
@@ -36,8 +36,16 @@ public class PracticeFotmTest {
 
         $("body > div.fade.modal.show > div > div > div.modal-body > div > table > tbody > tr:nth-child(1) > td:nth-child(2)")
                 .shouldHave(Condition.text("Alex Ivanov"));
-
-
+        $("body > div.fade.modal.show > div > div > div.modal-body > div > table > tbody > tr:nth-child(4) > td:nth-child(2)")
+                .shouldHave(Condition.text("1234567890"));
+        $("body > div.fade.modal.show > div > div > div.modal-body > div > table > tbody > tr:nth-child(9) > td:nth-child(2)")
+                .shouldHave(Condition.text("Address Taganrog"));
+        $("body > div.fade.modal.show > div > div > div.modal-body > div > table > tbody > tr:nth-child(2) > td:nth-child(2)")
+                .shouldHave(Condition.text("123@123.io"));
+        $("body > div.fade.modal.show > div > div > div.modal-body > div > table > tbody > tr:nth-child(6) > td:nth-child(2)")
+                .shouldHave(Condition.text("English"));
+        $("body > div.fade.modal.show > div > div > div.modal-body > div > table > tbody > tr:nth-child(7) > td:nth-child(2)")
+                .shouldHave(Condition.text("Music"));
 
 
     }
